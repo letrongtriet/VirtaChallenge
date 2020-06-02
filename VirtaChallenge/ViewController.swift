73c7d146fbf1528cd68cfc4538bd6067ba26326e
Triet Le <letrongtriet@gmail.com>
2020-06-02 12:00:03 +0300

    Initial Commit

---
+++ VirtaChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtaChallenge
+//
+//  Created by Triet Le on 2.6.2020.
+//  Copyright © 2020 Le Trong Triet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
